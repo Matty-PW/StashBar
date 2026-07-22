4e64a78a859a05c89a255c48a64b255e807e40e8
matty <mattyprinkuwright@gmail.com>
2026-07-22 13:34:22 -0700

    Initial Commit

---
+++ StashBar/StashBarApp.swift
@@ -0,0 +1,32 @@
+//
+//  StashBarApp.swift
+//  StashBar
+//
+//  Created by Matty on 22/07/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StashBarApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ StashBarTests/StashBarTests.swift
@@ -0,0 +1,19 @@
+//
+//  StashBarTests.swift
+//  StashBarTests
+//
+//  Created by Matty on 22/07/2026.
+//
+
+import Testing
+@testable import StashBar
+
+struct StashBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
